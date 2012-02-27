f0864ca8844e02e5b1773cb7f9cfc7046ebba9d8
Drew Roos <droos@dimagi.com>
2012-02-27 10:35:20 -0300

    support fetching keystore certs from urls; including OC server url by default

--- proxy/gen_client_keystore.py
+++ proxy/gen_client_keystore.py
@@ -3,6 +3,8 @@ import os
 import os.path
 import sys
 import tempfile
+import ssl
+from urlparse import urlparse
 
 # will create a client keystore with the debug and production certificates
 # specified in the settings file. the keystore will be written to the file
@@ -13,19 +15,43 @@ import tempfile
 KEYSTORE = 'keystore.bks'
 SHAM_PASSWORD = 'password'
 
-def add_cert(i, cert):
-    if os.path.samefile(cert, settings.DEFAULT_SSL_CERT):
-        id = 'debug'
-    else:
-        id = 'prod%d' % i
+def tmp_pemfile():
+    return tempfile.mkstemp(suffix='.pem')[1]
 
-    pemfile = tempfile.mkstemp(suffix='.pem')[1]
-    os.popen('openssl x509 -in %s -out %s' % (cert, pemfile))
-    os.popen('keytool -importcert -noprompt -alias %s -file %s -keystore %s -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath %s -storepass %s' % (id, pemfile, KEYSTORE, settings.BOUNCYCASTLE_JAR, SHAM_PASSWORD))
+def add_cert(i, cert):
+    if cert.startswith('http') and '://' in cert:
+        pemfile, namefunc = cert_from_web(cert)
+    else:
+        pemfile, namefunc = cert_from_file(cert)
+
+    os.popen('keytool -importcert -noprompt -alias %s -file %s -keystore %s -storetype BKS -provider org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath %s -storepass %s' % (namefunc(i), pemfile, KEYSTORE, settings.BOUNCYCASTLE_JAR, SHAM_PASSWORD))
+
+def cert_from_file(path):
+    def cert_id(i):
+        if os.path.samefile(path, settings.DEFAULT_SSL_CERT):
+            return 'debug'
+        else:
+            return 'prod%d' % i
+
+    pemfile = tmp_pemfile()
+    os.popen('openssl x509 -in %s -out %s' % (path, pemfile))
+    return pemfile, cert_id
+
+def cert_from_web(url):
+    up = urlparse(url)
+    cert = ssl.get_server_certificate((up.hostname, up.port or 443))
+    # fix formatting for certificate so keytool likes it
+    cert = '\n-----END CERT'.join(cert.split('-----END CERT'))
+
+    pemfile = tmp_pemfile()
+    with open(pemfile, 'w') as f:
+        f.write(cert)
+
+    return pemfile, lambda i: 'web%d' % i
 
 if __name__ == "__main__":
 
-    certs = set(crt for crt in [settings.DEFAULT_SSL_CERT, settings.SSL_CERT] if crt is not None)
+    certs = set(crt for crt in [settings.DEFAULT_SSL_CERT, settings.SSL_CERT, settings.OPENCLINICA_SERVER] if crt is not None)
     certs |= set(sys.argv[1:])
 
     if os.path.exists(KEYSTORE):
